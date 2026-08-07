ef3c69e869b60c39e19a6b16c6130bb9070324f1
leomin <minhtd721@gmail.com>
2026-08-07 23:49:05 +0700

    fix nvim

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -2,15 +2,17 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
   # boot.loader = {
   #     efi = {
-  #     canTouchEfiVariables = true;
+  #       canTouchEfiVariables = true;
   #     };
-  #     grub = {
-  #     enable = true;
-  #     efiSupport = true;
-  #     device = "nodev";
-  #     useOSProber = true;
+  #       grub = {
+  #       enable = true;
+  #       efiSupport = true;
+  #       device = "nodev";
+  #       useOSProber = true;
   #     };
   # };
   # boot.kernelParams = [ "acpi_backlight=vendor" ];
