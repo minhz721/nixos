50fdd7961db6bbc7a9f15376b5e1440fe02e0395
leomin <minhtd721@gmail.com>
2026-08-07 23:49:05 +0700

    fix nvim

--- home-manager/nixvim/plugins/fzf-lua.nix
+++ home-manager/nixvim/plugins/fzf-lua.nix
@@ -17,5 +17,63 @@
         };
       };
     };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>FzfLua files<CR>";
+        options.desc = "Find files";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = "<cmd>FzfLua live_grep<CR>";
+        options.desc = "Search project";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fb";
+        action = "<cmd>FzfLua buffers<CR>";
+        options.desc = "Find buffers";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fh";
+        action = "<cmd>FzfLua helptags<CR>";
+        options.desc = "Search help tags";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fr";
+        action = "<cmd>FzfLua oldfiles<CR>";
+        options.desc = "Recent files";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fw";
+        action = "<cmd>FzfLua grep_cword<CR>";
+        options.desc = "Search current word";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fc";
+        action = "<cmd>FzfLua commands<CR>";
+        options.desc = "Search commands";
+      }
+
+      {
+        mode = "n";
+        key = "<leader>fk";
+        action = "<cmd>FzfLua keymaps<CR>";
+        options.desc = "Search keymaps";
+      }
+    ];
   };
 }
