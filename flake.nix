f99130eea44c5958d779f57a2668e52baa0358bb
leomin <minhtd721@gmail.com>
2026-08-13 06:12:49 +0700

    fix float window

--- flake.nix
+++ flake.nix
@@ -23,10 +23,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-xdm = {
-      url = "github:Diomeh/nix-xdm";
-      flake = false;
-    };
+    # nix-xdm = {
+    #   url = "github:Diomeh/nix-xdm";
+    #   flake = false;
+    # };
   };
 
   outputs = {
@@ -34,7 +34,7 @@
     home-manager,
     custom-packages,
     nixvim,
-    nix-xdm,
+    # nix-xdm,
     ...
   } @ inputs: let
     system = "x86_64-linux";
--- modules/packages.nix
+++ modules/packages.nix
@@ -60,6 +60,6 @@
     # External Packages
     # =========================================================================
     inputs.custom-packages.packages."x86_64-linux".thorium-avx2
-    # inputs.custom-packages.packages."x86_64-linux".ab-download-manager
+    inputs.custom-packages.packages."x86_64-linux".ab-download-manager
   ];
 }
