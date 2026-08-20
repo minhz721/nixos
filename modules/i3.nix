af9216d3c34926844291886725936e125990c39f
leomin <minhtd721@gmail.com>
2026-08-20 22:00:36 +0700

    add i3vm

---
+++ modules/i3.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  callPackage,
+  ...
+}: {
+  services.xserver = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+    };
+    displayManager.lightdm = {
+      enable = true;
+    };
+  };
+
+  services.displayManager.defaultSession = "none+i3";
+
+  services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.xrandr}/bin/xrandr \
+        --output DisplayPort-0 --mode 2560x1440 --rate 200 --primary --pos 0x0 \
+        --output HDMI-A-0 --mode 1920x1080 --rate 60 --pos 320x1440
+  '';
+
+  environment.systemPackages = with pkgs; [
+    rofi
+    picom
+    dunst
+    feh
+    lightlocker
+    xclip
+    font-awesome 
+    i3status-rust
+  ];
+}
--- modules/packages.nix
+++ modules/packages.nix
@@ -43,9 +43,9 @@
     # Development
     # =========================================================================
     # git
-    python3
+    python314
     nodejs
-    tk
+    python314Packages.tkinter
     dotnetCorePackages.sdk_8_0
 
     # =========================================================================
@@ -60,8 +60,6 @@
     inputs.custom-packages.packages."x86_64-linux".thorium-avx2
     # inputs.custom-packages.packages."x86_64-linux".ab-download-manager
     gnome-themes-extra
-    adwaita-icon-theme 
-
+    adwaita-icon-theme
   ];
-
 }
